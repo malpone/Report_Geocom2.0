867be796f84d4c31286310edbef42379b776eb4b
DANIELE MALPASSI <malpone@gmail.com>
2026-01-12 02:05:33 +0100

    Add files via upload

---
+++ AVVIA_REPORT.bat
@@ -0,0 +1,19 @@
+B. Materiali Marketing e Collaterali
+Produzione In-Loco
+Costi vivi per stampa brochure in lingua (Inglese/Cinese)
+Stampa biglietti da visita con QR code
+Opzione di spedizione materiale direttamente presso fiera
+N.B.
+: Traduzione dei testi è inclusa nei Servizi Offerti 
+
+
+C. Logistica di Missione (Travel & Living)
+
+Gestione costi vivi di trasferta:
+
+Voli aerei e trasferimenti locali: aeroporto → hotel → fiera
+Pernottamento: alloggio in hotel di categoria business
+Vitto e cene di rappresentanza: meals e business dinners per relazioni commerciali
+
+
+
